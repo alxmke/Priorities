6ff0e1a8cb5fa884e6f45f007b9ee82cba0e37b7
Alex Mackey <alxmke@berkeley.edu>
2019-08-02 22:04:30 -0700

    current test case, beginning to generalize

--- group_priority.py
+++ group_priority.py
@@ -1,63 +1,62 @@
 from collections import defaultdict
 import operator
 c1 = {
-  "head":[233.99,"Helm of the Claw"],
+  "head":[301.87,"Wastewalker Helm"], #T4 BIS
   "neck":[97.65,"Natasha's Choker"],
-  "shoulder":[100.0,""],
-  "back":[89.88,"Delicate Green Poncho"],
+  "shoulder":[239.02,"Wastewalker Shoulderpads"], #T4 BIS
+  "back":[96.22,"Cloak of the Inciter"],
   "chest":[160.48,"Auchenai Monk's Tunic"],
-  "wrist":[97.84,"Nightfall Wristguards"],
+  "wrist":[97.84,"Nightfall Wristguards"], #T4 2nd BIS
   "hands":[150.26,"Fel Leather Gloves"], #T4 BIS
   "waist":[161.68,"Girdle of the Deathdealer"], #T4 BIS
   "legs":[204.09,"Fel Leather Leggings"],
   "feet":[157.19,"Edgewalker Longboots"], #T4 BIS
   "finger":[105.0,"Slayer's Mark of Redemption"],
-  "trinket":[43.0,"Core of Ar'kelos"],
-  "weapons":[252.0,"Reflex Blades (Fist)"],
+  "trinket":[59.0,"Bloodlust Brooch"],
+  "weapons":[276.0,"Vindicator's Brand"],
   "ranged":[63.62,"Mama's Insurance (Gun)"]
 }
 
 c2 = {
-  "head":[79.15,"Hood of Oblivion"],
-  "neck":[38.9,"Natasha's Ember Necklace"],
-  "shoulder":[54.85,"Mantle of Three Terrors"],
+  "head":[126.08,"Voidheart Crown"],
+  "neck":[54.09,"Brooch of Heightened Potential"],
+  "shoulder":[75.05,"Frozen Shadoweave Shoulders"],
   "back":[44.5,"Sethekk Oracle Cloak"],
   "chest":[92.2,"Auchenai Anchorite's Robe"],
-  "wrist":[36.75,"Shattrath Wraps"],
-  "hands":[54.1,"Tempest's Touch"],
-  "waist":[50.19,"A'dal's Gift"],
-  "legs":[50.0,"Devil-stitched Leggins"],
-  "feet":[55.05,"Shattrath Jumpers"],
-  "finger":[34.8,"Spectral Band of Innervation"],
-  "trinket":[23.9,"Vengeance of the Illidari"],
+  "wrist":[44.5,"Crimson Bracers of Gloom"],
+  "hands":[109.96,"Voidheart Gloves"],
+  "waist":[66.75,"Girdle of Ruination"],
+  "legs":[70.84,"Aran's Sorcerous Slacks"],
+  "feet":[55.05,"79.35"],
+  "finger":[46.37,"Violet Signet of the Archmage"],
+  "trinket":[53.8,"Quagmirran's Eye"],
   "weapons":[136.5+46.72,"Continuum Blade; Lamp of Peaceful Raidiance"],
-  "ranged":[16.95,"Wand of the Netherwing"]
+  "ranged":[34.16,"Tirisfal Wand of Ascendancy"]
 }
 
 c3 = {
-  "head":[90.0,""],
+  "head":[193.72,"Hallowed Crown"],
   "neck":[93.5,"Necklace of Resplendent Hope"],
-  "shoulder":[110.2,"Vestia's Pauldrons of Inner Grace"],
+  "shoulder":[132.38,"Primal Mooncloth Shoulders"],
   "back":[89.5,"Vicar's Cloak"],
   "chest":[146.55,"Void Slayer's Tunic"],
   "wrist":[87.8,"Whirlwind Bracers"], #BIS T4
-  "hands":[55.0,""],
-  "waist":[75.0,""],
-  "legs":[144.51,"Hallowed Trousers"],
+  "hands":[112.7,"Fathomheart Gauntlets"],
+  "waist":[151.84,"Primal Mooncloth Belt"],
+  "legs":[200.55,"Cyclone Kilt"],
   "feet":[103.11,"Light-Woven Slippers"],
   "finger":[80.0,""],
   "trinket":[0.0,""],
-  "weapons":[315.85,"The Epoch Mender"],
-  "ranged":[0.0,""]
+  "weapons":[368.5+100,"Hand of Eternity; Aran's Soothing Sapphire"],
+  "ranged":[10.0,"Totem of Spontaneous Regrowth; Totem of the Plains"]
 }
 
 c1_n = [
-  ("head",301.87,"H OHF","Wastewalker Helm"), #T4 BIS
-  ("shoulder",239.02,"H AC","Wastewalker Shoulderpads"), #T4 BIS
+#  ("head",301.87,"H OHF","Wastewalker Helm"), #T4 BIS
+#  ("shoulder",239.02,"H AC","Wastewalker Shoulderpads"), #T4 BIS
   ("back",106.82,"H AC","Auchenai Death Shroud"),
-  ("chest",179.02,"BF","Vest of Vengeance"),
-  ("chest",179.02,"H BF","Vest of Vengeance"),
-#  ("wrist",97.84,"H OHF","Nightfall Wristguards"), #T4 BIS
+  ("chest",182.08,"H BF","Wastewalker Tunic"),
+#  ("wrist",97.84,"H OHF","Nightfall Wristguards"), #T4 2nd BIS
 #  ("waist",161.68,"H BM","Girdle of the Deathdealer"), #T4 BIS
 #  ("trinket",59.0,"H BM","Hourglass of the Unraveller"),
 #  ("trinket",59.0,"BM","Hourglass of the Unraveller"),
@@ -67,17 +66,16 @@ c1_n = [
 ] #(item_place, value, item_location, item_id)
 c2_n = [
   ("neck",45.45,"H UB","Hydra-fang Necklace"),
-  ("shoulder",94.29,"H UB","Mana-Etched Spaulders"),
- # ("back",44.5,"SH","Sethekk Oracle Cloak"),
- # ("chest",92.2,"AC","Auchenai Anchorite's Robe"),
-  ("wrist",44.5,"H Ramps","Crimson Bracers of Gloom"),
+# ("back",44.5,"SH","Sethekk Oracle Cloak"),
+# ("chest",92.2,"AC","Auchenai Anchorite's Robe"),
+# ("wrist",44.5,"H Ramps","Crimson Bracers of Gloom"),
   ("hands",94.29,"H Ramps", "Mana-Etched Gloves"),
   ("waist",66.75,"H Arc","Belt of Depravity"),
   ("legs",74.3,"H BM","Khadgar's Kilt of Abjuration"),
   ("legs",74.3,"BM","Khadgar's Kilt of Abjuration"),
-  ("finger",45.71,"H OHF","Sparking Arcanite Ring"),
-  ("finger",42.75,"H MT","Cobalt Band of Tyrigosa"),
-  ("trinket",53.8,"H SP","Quagmirran's Eye"),
+#  ("finger",45.71,"H OHF","Sparking Arcanite Ring"),
+  ("finger",42.7,"H MT","Cobalt Band of Tyrigosa"),
+#  ("trinket",53.8,"H SP","Quagmirran's Eye"),
   ("trinket",44.5,"H OHF","Arcanist's Stone"),
   ("weapons",153.15+44.45,"SLabs","Greatsword of Horrid Dreams"),
   ("weapons",153.15+44.45,"H SLabs","Greatsword of Horrid Dreams"),
@@ -85,20 +83,21 @@ c2_n = [
   ("ranged",27.05,"Arc","Nether Core's Control Rod")
 ] #(item_place, value, item_location, item_id)
 c3_n = [
-  ("head",193.72,"Arc","Hallowed Crown"),
-  ("head",193.72,"H Arc","Hallowed Crown"),
+#  ("head",193.72,"Arc","Hallowed Crown"),
+#  ("head",193.72,"H Arc","Hallowed Crown"),
 #  ("neck",93.5,"H OHF","Necklace of Resplendent Hope"),
   ("shoulder",113.1,"H MT","Mantle of the Sea Wolf"),
 #  ("back",99.8,"BOJ","Bishop's Cloak"),
-  ("hands",112.7,"SV","Fathomheart Gauntlets"),
+#  ("hands",112.7,"SV","Fathomheart Gauntlets"),
   ("waist",118.75,"H OHF","Cord of Sanctification"),
- # ("legs",144.51,"SH","Hallowed Trousers"),
+# ("legs",144.51,"SH","Hallowed Trousers"),
   ("feet",115.95,"H SP","Wavefury Boots"),
+  ("feet",108.1,"H ShH","Jeweled Boots of Sanctification"),
   ("feet",108.1,"ShH","Jeweled Boots of Sanctification"),
 #  ("feet",103.11,"SH","Light-Woven Slippers"),
   ("trinket",50.0,"Bot","Bangle of Endless Blessings"),
   ("weapons",413.2,"H SLabs","Shockwave Truncheon"),
-  ("ranged",10.0,"H SP","Totem of Spontaneous Regrowth")
+#  ("ranged",10.0,"H SP","Totem of Spontaneous Regrowth")
 ] #(item_place, value, item_location, item_id)
 
 class Member:
@@ -171,9 +170,10 @@ class Member:
     for e in self.upgrades:
       part,location,name,value = e
       if part in self.current:
-        d = max(value-self.current[part][1],0)
-        destinations[location] += d
-        destination_items[location] += [name]
+        d = value-self.current[part][1]
+        if d>0:
+          destinations[location] += d
+          destination_items[location] += [name]
     return [(k,v,destination_items[k]) for k,v in sorted(destinations.items(),key=operator.itemgetter(1))]
 
 # the following import helper functions have their file structures defined above in Member (i.e. weight file a tsv OTF etc.)
@@ -185,7 +185,6 @@ def import_weights(w):
     print("Input error: Empty weights file")
     return None
   while l != ['']:
-    print("Debugging",l)
     stat_name,stat_value = l
     weights[stat_name] = float(stat_value)
     l = w.readline().rstrip().split('\t')
@@ -199,7 +198,6 @@ def import_upgrades(u, weights):
     print("Input error: Empty upgrades file")
     return None
   while l != ['']:
-    print("Debugging",l)
     part,location,name,n = l
     stats = dict()
     for i in range(int(n)):
@@ -245,20 +243,23 @@ def hc_group_priority(group):
     for e in upgrades:
       part,value,location,name = e
       if part in member:
-        d = max(value-member[part][0],0)
-        destinations[location] += d
-        destination_items[location] += [name]
+        d = value-member[part][0]
+        if(d>0):
+          destinations[location] += d
+          destination_items[location] += [name]
   return [(k,v,destination_items[k]) for k,v in sorted(destinations.items(),key=operator.itemgetter(1))]
 
-# original test from hard-coded values
-#group = [(c1,c1_n),(c2,c2_n),(c3,c3_n)]
-#g = hc_group_priority(group)
-#g.reverse()
-#for m in g:
-#  location,value,items=m
-#  print(f"Value of {round(value)} gained from {location}")
-#  print(f"\tfrom: {items}")
+#tests
+# original test from hard-coded values, for priority calculator(s)
+group = [(c1,c1_n),(c2,c2_n),(c3,c3_n)]
+g = hc_group_priority(group)
+g.reverse()
+for m in g:
+  location,value,items=m
+  print(f"Value of {round(value)} gained from {location}")
+  print(f"\tfrom: {items}")
 
+# test for import member from file
 M = Member.From_file("weights.dat","upgrades.dat","current.dat")
 if M is None:
   print("Error importing member data.")
